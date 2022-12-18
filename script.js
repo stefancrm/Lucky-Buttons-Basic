1473f1a01d62e33f3420d86974cd49940367d769
Stefan_Caramizoiu <stefancaramizoiu@gmail.com>
2022-12-18 20:09:57 +0200

    update core functionality

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// function for guessing the lucky number
-function guess(n) {
-	console.log(n + " " + luckyNumber());
-	return n == luckyNumber() ? 
-		window.alert("You guessed it right\nCongratulations!") : 
-		window.alert("You guessed wrong\nTry Again!")
-}
 //Generate lucky number
 function luckyNumber() {
 	return Math.floor(Math.random() * 3 + 1);
 }
+let luckyNr
+//on page load generate lucky number
+window.onload = function() {
+	luckyNr = luckyNumber()
+	console.log(luckyNr)
+  };
+
+// function for guessing the lucky number
+function guess(n) {
+	console.log(n + " " + luckyNr);
+	return n == luckyNr ? 
+		window.alert("You guessed it right\nCongratulations!") : 
+		window.alert("You guessed wrong\nTry Again!")
+}
+
